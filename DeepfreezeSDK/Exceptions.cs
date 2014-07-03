f911d0829767cc14e76cfbe3c5ad6fb5c582c446
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-03 12:04:11 +0300

    more exceptions.

--- DeepfreezeSDK/Exceptions.cs
+++ DeepfreezeSDK/Exceptions.cs
@@ -22,6 +22,34 @@ namespace DeepfreezeSDK
             { }
         }
 
+        public class NoArchivesFoundException : Exception
+        {
+            public NoArchivesFoundException()
+                : base("Could not fetch any archives.") { }
+
+            public NoArchivesFoundException(string message)
+                : base(message)
+            { }
+
+            public NoArchivesFoundException(string message, Exception inner)
+                : base(message, inner)
+            { }
+        }
+
+        public class NoUploadsFoundException : Exception
+        {
+            public NoUploadsFoundException()
+                : base("Could not fetch any archives.") { }
+
+            public NoUploadsFoundException(string message)
+                : base(message)
+            { }
+
+            public NoUploadsFoundException(string message, Exception inner)
+                : base(message, inner)
+            { }
+        }
+
         public class CreateTokenException : Exception
         {
             public CreateTokenException()
@@ -32,6 +60,34 @@ namespace DeepfreezeSDK
             { }
 
             public CreateTokenException(string message, Exception inner)
+                : base(message, inner)
+            { }
+        }
+
+        public class CreateArchiveException : Exception
+        {
+            public CreateArchiveException()
+                : base("Could not create a new archive.") { }
+
+            public CreateArchiveException(string message)
+                : base(message)
+            { }
+
+            public CreateArchiveException(string message, Exception inner)
+                : base(message, inner)
+            { }
+        }
+
+        public class CreateUploadException : Exception
+        {
+            public CreateUploadException()
+                : base("Could not create a new upload.") { }
+
+            public CreateUploadException(string message)
+                : base(message)
+            { }
+
+            public CreateUploadException(string message, Exception inner)
                 : base(message, inner)
             { }
         }
