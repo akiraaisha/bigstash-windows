7d1dff6be6d3882c01ff0b29a6af4487bb63b7b0
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-03 12:03:49 +0300

    add quota.

---
+++ DeepfreezeModel/Quota.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DeepfreezeModel
+{
+    public class Quota
+    {
+        [JsonProperty("size")]
+        public long Size { get; set; }
+
+        [JsonProperty("used")]
+        public long Used { get; set; }
+
+        public Quota() { }
+
+        public Quota(JObject json)
+        {
+            this.Size = (long)json["size"];
+            this.Used = (long)json["used"];
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
