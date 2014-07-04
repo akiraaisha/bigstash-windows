7d6a2cb45ec3bf40a21033203a54347cfe9ff7c4
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-04 13:18:29 +0300

    remove user from token, add json properties for serializing purposes.

--- DeepfreezeModel/Token.cs
+++ DeepfreezeModel/Token.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace DeepfreezeModel
 {
@@ -13,20 +14,26 @@ namespace DeepfreezeModel
         /// Authorization token for communicating with
         /// the Deepfreeze API and Amazon S3 API.
         /// </summary>
+        [JsonProperty("key")]
         public string Key { get; set; }
         /// <summary>
         /// Unique token ID.
         /// </summary>
+        [JsonProperty("id")]
         public int ID { get; set; }
+        /// <summary>
+        /// Secret key.
+        /// </summary>
+        [JsonProperty("secret")]
+        public string Secret { get; set; }
         /// <summary>
         /// Token creation date.
         /// </summary>
-        public DateTime Created { get; set; }
-        public string User { get; set; }
-        public string Secret { get; set; }
+        [JsonProperty("created")]
+        public DateTime CreatedDate { get; set; }
 
         /// <summary>
-        /// Serialize Token to JSON string
+        /// Serialize TokenPostResponse to JSON string
         /// </summary>
         /// <returns></returns>
         public string ToJson()
