63562f566887ed7704af477dea31deef6dbd8f26
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-05 19:38:10 +0300

    add preferences viewmodel, view, interface.

---
+++ DeepfreezeApp/ViewModels/PreferencesViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+using Caliburn.Micro;
+using DeepfreezeSDK;
+
+namespace DeepfreezeApp
+{
+    [Export(typeof(IPreferencesViewModel))]
+    public class PreferencesViewModel : PropertyChangedBase, IPreferencesViewModel
+    {
+        #region members
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IDeepfreezeClient _deepfreezeClient;
+
+        private IUserViewModel _userVM = IoC.Get<IUserViewModel>();
+
+        private bool _isOpen;
+        #endregion
+
+        #region constructors
+        public PreferencesViewModel() { }
+
+        public PreferencesViewModel(IEventAggregator eventAggregator, IDeepfreezeClient deepfreezeClient)
+        {
+            this._eventAggregator = eventAggregator;
+            this._deepfreezeClient = deepfreezeClient;
+        }
+        #endregion
+
+        #region properties
+        public UserViewModel UserVM
+        {
+            get { return _userVM as UserViewModel; }
+        }
+
+        public bool IsOpen
+        {
+            get { return this._isOpen; }
+            set { this._isOpen = value; NotifyOfPropertyChange(() => IsOpen); }
+        }
+        #endregion
+
+        #region methods
+
+        #endregion
+    }
+}
--- DeepfreezeApp/ViewModels/ShellViewModel.cs
+++ DeepfreezeApp/ViewModels/ShellViewModel.cs
@@ -7,28 +7,69 @@ using System.ComponentModel.Composition;
 
 using Caliburn.Micro;
 using DeepfreezeSDK;
+using MahApps.Metro.Controls;
 
 namespace DeepfreezeApp
 {
     [Export(typeof(IShell))]
-    public class ShellViewModel : Screen, IShell
+    public class ShellViewModel : Screen, IShell, IHandle<ILoginSuccessMessage>
     {
+        #region fields
         private readonly IWindowManager _windowManager;
         private readonly IEventAggregator _eventAggregator;
         private readonly IDeepfreezeClient _deepfreezeClient;
 
         private IArchiveViewModel _archiveVM = IoC.Get<IArchiveViewModel>();
+        private ILoginViewModel _loginVM = IoC.Get<ILoginViewModel>();
+        private IPreferencesViewModel _preferencesVM = IoC.Get<IPreferencesViewModel>();
+
+        private MetroWindow _shellWindow;
+        #endregion
+
+        #region properties
+        public MetroWindow ShellWindow
+        {
+            get { return this._shellWindow; }
+        }
+        public bool IsLoggedIn
+        { get { return this._deepfreezeClient.IsLogged(); } }
+        
         public ArchiveViewModel ArchiveVM
         {
             get { return this._archiveVM as ArchiveViewModel; }
         }
-
-        private ILoginViewModel _loginVM = IoC.Get<ILoginViewModel>();
+        
         public LoginViewModel LoginVM
         {
             get { return this._loginVM as LoginViewModel; }
         }
 
+        public PreferencesViewModel PreferencesVM
+        {
+            get { return this._preferencesVM as PreferencesViewModel; }
+        }
+
+        public bool IsPreferencesFlyoutOpen
+        {
+            get { return this.PreferencesVM.IsOpen; }
+            set { this.PreferencesVM.IsOpen = value; NotifyOfPropertyChange(() => IsPreferencesFlyoutOpen); }
+        }
+
+        public string PreferencesHeader
+        { get { return Properties.Resources.PreferencesHeader; } }
+
+        #endregion
+
+        #region action methods
+        public void TogglePreferencesFlyout()
+        {
+            ShellWindow.ShowWindowCommandsOnTop = IsPreferencesFlyoutOpen;
+            IsPreferencesFlyoutOpen = !IsPreferencesFlyoutOpen;
+            
+        }
+        #endregion
+
+        #region constructors
         public ShellViewModel() { }
 
         [ImportingConstructor]
@@ -37,6 +78,30 @@ namespace DeepfreezeApp
             this._windowManager = windowManager;
             this._eventAggregator = eventAggregator;
             this._deepfreezeClient = deepfreezeClient;
+
+            this._eventAggregator.Subscribe(this);
+
+            //IsLoggedIn = _deepfreezeClient.IsLogged();
         }
+        #endregion
+
+        #region message handlers
+        public void Handle(ILoginSuccessMessage message)
+        {
+            NotifyOfPropertyChange(() => IsLoggedIn);
+        }
+        #endregion
+
+        #region events
+        protected override void OnViewLoaded(object view)
+        {
+            var v = view as MetroWindow;
+            v.Title = Properties.Settings.Default.ApplicationName;
+            if (v != null)
+                _shellWindow = v;
+
+            base.OnViewLoaded(view);
+        }
+        #endregion
     }
 }
