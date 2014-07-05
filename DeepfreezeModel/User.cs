dc89d6113800ce085a92aaa71e06117d823e1510
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-05 20:38:54 +0300

    add comments, json serializing attributes, map to API user.

--- DeepfreezeModel/User.cs
+++ DeepfreezeModel/User.cs
@@ -12,22 +12,38 @@ namespace DeepfreezeModel
         /// <summary>
         /// User ID.
         /// </summary>
+        [JsonProperty("id")]
         public int ID { get; set; }
 
         /// <summary>
         /// User's email.
         /// </summary>
+        [JsonProperty("email")]
         public string Email { get; set; }
 
         /// <summary>
         /// Date when user joined the Deepfreeze service.
         /// </summary>
+        [JsonProperty("date_joined")]
         public DateTime DateJoined { get; set; }
 
         /// <summary>
         /// User's Display Name.
         /// </summary>
+        [JsonProperty("displayname")]
         public string DisplayName { get; set; }
+
+        /// <summary>
+        /// User's Archives.
+        /// </summary>
+        [JsonProperty("archives")]
+        public List<Archive> Archives { get; set; }
+
+        /// <summary>
+        /// User's Quota.
+        /// </summary>
+        [JsonProperty("quota")]
+        public Quota Quota { get; set; }
 
         /// <summary>
         /// Serialize User to JSON string
