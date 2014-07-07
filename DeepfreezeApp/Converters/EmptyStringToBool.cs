7cbfe0f7a5da3b1b7029b0f5e55b472da228fa63
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-07 19:23:25 +0300

    add emtpy string to bool converter

---
+++ DeepfreezeApp/Converters/EmptyStringToBool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DeepfreezeApp
+{
+    class EmptyStringToBool : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string s = (string)value;
+
+            return String.IsNullOrEmpty(s) || String.IsNullOrWhiteSpace(s);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
