663f92b388c3c2d27b20d0ed78b25ab48d9ff9fb
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-07 19:34:39 +0300

    add upload manager vm and view, add upload vm and view

---
+++ DeepfreezeApp/ViewModels/UploadManagerViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+using Caliburn.Micro;
+using DeepfreezeSDK;
+using DeepfreezeModel;
+
+namespace DeepfreezeApp
+{
+    [Export(typeof(IUploadManagerViewModel))]
+    public class UploadManagerViewModel : IUploadManagerViewModel
+    {
+        #region fields
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IDeepfreezeClient _deepfreezeClient;
+        #endregion
+
+        #region constructor
+        [ImportingConstructor]
+        public UploadManagerViewModel(IEventAggregator eventAggregator, IDeepfreezeClient deepfreezeClient)
+        {
+            this._eventAggregator = eventAggregator;
+            this._deepfreezeClient = deepfreezeClient;
+        }
+
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region action methods
+        #endregion
+
+        #region message handlers
+        #endregion
+
+        #region private methods
+        #endregion
+    }
+}
---
+++ DeepfreezeApp/ViewModels/UploadViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+using Caliburn.Micro;
+using DeepfreezeSDK;
+using DeepfreezeModel;
+
+namespace DeepfreezeApp
+{
+    [Export(typeof(IUploadViewModel))]
+    public class UploadViewModel : IUploadViewModel
+    {
+        #region fields
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IDeepfreezeClient _deepfreezeClient;
+        #endregion
+
+        #region constructor
+        [ImportingConstructor]
+        public UploadViewModel(IEventAggregator eventAggregator, IDeepfreezeClient deepfreezeClient)
+        {
+            this._eventAggregator = eventAggregator;
+            this._deepfreezeClient = deepfreezeClient;
+        }
+
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region action methods
+        #endregion
+
+        #region message handlers
+        #endregion
+
+        #region private methods
+        #endregion
+    }
+}
