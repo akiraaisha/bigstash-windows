c8a4e43d949d7d3a74f104e573406760a3c67186
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-07 22:48:29 +0300

    add LocalUploads to hold upload urls started from this client.

---
+++ DeepfreezeModel/LocalUploads.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace DeepfreezeModel
+{
+    [JsonObject("local_uploads")]
+    public class LocalUploads
+    {
+        private IList<string> _urls;
+        [JsonProperty("urls")]
+        public IList<string> Urls
+        {
+            get { return this._urls; }
+            set { this._urls = value; }
+        }
+    }
+}
