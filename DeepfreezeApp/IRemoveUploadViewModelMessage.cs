79908951e56cb2de5567586346880e17664b0696
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-15 13:33:10 +0300

    add UploadViewModel property.

--- DeepfreezeApp/IRemoveUploadViewModelMessage.cs
+++ DeepfreezeApp/IRemoveUploadViewModelMessage.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace DeepfreezeApp
 {
-    interface IRemoveUploadViewModelMessage
+    public interface IRemoveUploadViewModel
     {
+        UploadViewModel UploadVMToRemove { get; set; }
     }
 }
