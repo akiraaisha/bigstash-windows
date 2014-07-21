0c136ef8430266b0ace8c9f0419d92267e12c607
Theofanis Tokas <tokastheofanis@gmail.com>
2014-07-21 14:18:20 +0300

    add Progress property to save current progress to local upload file.

--- DeepfreezeModel/LocalUpload.cs
+++ DeepfreezeModel/LocalUpload.cs
@@ -10,11 +10,20 @@ namespace DeepfreezeModel
 {
     public class LocalUpload
     {
+        private long _progress = 0;
+
         [JsonIgnore]
         public string SavePath { get; set; }
 
         [JsonProperty("url")]
         public string Url { get; set; }
+
+        [JsonProperty("progress")]
+        public long Progress
+        {
+            get { return this._progress; }
+            set { this._progress = (long)value; }
+        }
 
         [JsonProperty("archive_files_info")]
         public IList<ArchiveFileInfo> ArchiveFilesInfo { get; set; }
