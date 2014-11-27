a92fbc10a95384b3f8356f3cc322c98a6388fc45
Theofanis Tokas <tokastheofanis@gmail.com>
2014-11-27 17:09:28 +0200

    Add interface MetroWindow property ShellWindow, which holds a reference to the current main application window object.

--- DeepfreezeApp/Interfaces/IShell.cs
+++ DeepfreezeApp/Interfaces/IShell.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MahApps.Metro.Controls;
 
 namespace DeepfreezeApp
 {
     public interface IShell
     {
+        MetroWindow ShellWindow { get; }
     }
 }
