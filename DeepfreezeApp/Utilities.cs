a4c9c34e4ae781c2cfdda9b2581c65273a4ba294
Theofanis Tokas <tokastheofanis@gmail.com>
2014-11-28 11:23:16 +0200

    Add new static method "CheckFileApiRestrictions()" to check for the existence of files that should be excluded when creating a new archive.

--- DeepfreezeApp/Utilities.cs
+++ DeepfreezeApp/Utilities.cs
@@ -6,10 +6,25 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+using DeepfreezeModel;
+using System.Text.RegularExpressions;
+
 namespace DeepfreezeApp
 {
     public static class Utilities
     {
+        private static char[] invalidFileNameChars = Path.GetInvalidFileNameChars();
+        private static Regex xmlValidCharsRegEx = new Regex(@"[\u0009\u000a\u000d\u0020-\uD7FF\uE000-\uFFFD]");
+        private static IList<string> systemFilesToExlude = new List<string>()
+                                                            {
+                                                                "desktop.ini",
+                                                                "thumbs.db",
+                                                                ".ds_store",
+                                                                "icon\r",
+                                                                ".dropbox",
+                                                                ".dropbox.attr"
+                                                            };
+
         /// <summary>
         /// Returns the application's installation directory.
         /// </summary>
@@ -57,5 +72,164 @@ namespace DeepfreezeApp
                 }
             }
         }
+
+        /// <summary>
+        /// Identify if path is a junction point.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>bool</returns>
+        public static bool IsJunction(string path)
+        {
+            try
+            {
+                var attributes = File.GetAttributes(path);
+                bool isJunction = (attributes & FileAttributes.ReparsePoint) == FileAttributes.ReparsePoint;
+                return isJunction;
+            }
+            catch(Exception e)
+            { throw e; }
+        }
+
+        /// <summary>
+        /// Check if a file complies with the BigStash API restrictions for accepted file names.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>Enumerations.InvalidFileCategory</returns>
+        public static Enumerations.FileCategory CheckFileApiRestrictions(string path)
+        {
+            try
+            {
+                // check the file name character count
+                if (path.Count() > 260)
+                {
+                    return Enumerations.FileCategory.FileNameTooLong;
+                }
+
+                // check if the file does not exist.
+                if (!File.Exists(path))
+                {
+                    throw new System.IO.FileNotFoundException();
+                }
+
+                // get the file name
+                var fileName = Path.GetFileName(path).ToLower();
+
+                // check if the file is in the ignored system files list.
+                if (systemFilesToExlude.Contains(fileName))
+                {
+                    return Enumerations.FileCategory.IgnoredSystemFile;
+                }
+
+                // check if the filename ends with a period or a whitespace.
+                if (fileName.EndsWith(" ") || fileName.EndsWith("."))
+                {
+                    return Enumerations.FileCategory.TrailingPeriodsOrWhiteSpaceInName;
+                }
+
+                // check if the filename starts with characters used in naming
+                // temporary files. Such a file might not be an actual temporary file,
+                // but it will be excluded nonetheless.
+                if (fileName.StartsWith(@"~$") || fileName.StartsWith(@".~") ||
+                    (fileName.StartsWith("~") && fileName.EndsWith(".tmp")))
+                {
+                    return Enumerations.FileCategory.TemporaryFile;
+                }
+
+                // check for the existence of invalid file name chars
+                // OR if the name does not satisfy the pattern.
+                if (fileName.IndexOfAny(invalidFileNameChars) > -1 ||
+                    !xmlValidCharsRegEx.IsMatch(fileName))
+                {
+                    return Enumerations.FileCategory.InvalidCharacterInName;
+                }
+
+                // Get the file attributes.
+                FileAttributes attributes = File.GetAttributes(path);
+
+                // check if attributes point that the file is a temporary file.
+                if ((attributes & FileAttributes.Temporary) == FileAttributes.Temporary)
+                {
+                    return Enumerations.FileCategory.TemporaryFile;
+                }
+
+                if ((attributes & FileAttributes.Offline) == FileAttributes.Offline)
+                {
+                    return Enumerations.FileCategory.UnsyncedOnlineFile;
+                }
+
+                // check if attributes point that the file is a junction point
+                // of if it's a shortcut file.
+                if ((attributes & FileAttributes.ReparsePoint) == FileAttributes.ReparsePoint)
+                {
+                    return Enumerations.FileCategory.MetadataFile;
+                }
+
+                return Enumerations.FileCategory.Normal;
+            }
+            catch(Exception e)
+            { throw e; }
+        }
+
+        #region shell32_code
+        ///// <summary>
+        ///// Check if path is a shortcut, either a .lnk file or a .appref-ms.
+        ///// Other application specific shortcut files can also be identified.
+        ///// </summary>
+        ///// <param name="path"></param>
+        ///// <returns>bool</returns>
+        //private static bool IsShortcut(string path)
+        //{
+        //    try
+        //    {
+        //        if (!File.Exists(path))
+        //        {
+        //            throw new System.IO.FileNotFoundException();
+        //        }
+
+        //        string directory = Path.GetDirectoryName(path);
+        //        string file = Path.GetFileName(path);
+
+        //        // Shell32.Shell shell = new Shell32.Shell();
+        //        // Shell32.Folder folder = shell.NameSpace(directory);
+
+        //        //This call is needed to get the folder object from the Shell32 application under Windows 8.
+        //        Shell32.Folder folder = GetShell32NameSpaceFolder(directory); 
+                
+        //        if (folder == null)
+        //        {
+        //            return false;
+        //        }
+
+        //        Shell32.FolderItem folderItem = folder.ParseName(file);
+
+        //        if (folderItem != null)
+        //        {
+        //            return folderItem.IsLink;
+        //        }
+
+        //        return false;
+        //    }
+        //    catch(Exception e)
+        //    { 
+        //        // log
+        //        throw e; 
+        //    }
+        //}
+
+        ///// <summary>
+        ///// This method is needed to get the folder object from the Shell32 application
+        ///// under Windows 8.
+        ///// </summary>
+        ///// <param name="folder"></param>
+        ///// <returns></returns>
+        //private static Shell32.Folder GetShell32NameSpaceFolder(Object folder)
+        //{
+        //    Type shellAppType = Type.GetTypeFromProgID("Shell.Application");
+
+        //    Object shell = Activator.CreateInstance(shellAppType);
+        //    return (Shell32.Folder)shellAppType.InvokeMember("NameSpace",
+        //    System.Reflection.BindingFlags.InvokeMethod, null, shell, new object[] { folder });
+        //}
+        #endregion
     }
 }
