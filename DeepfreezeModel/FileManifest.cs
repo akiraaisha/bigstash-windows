626a76e19c5964ab10801d4a7281a722c074883a
Theofanis Tokas <tokastheofanis@gmail.com>
2014-12-17 19:46:52 +0200

    New model FileManifest to describe the json schema to be used for keeping the archive's files' info.

---
+++ DeepfreezeModel/FileManifest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DeepfreezeModel
+{
+    public class FileManifest
+    {
+        /// <summary>
+        /// Key name for the S3 Object.
+        /// </summary>
+        [JsonProperty("key_name")]
+        public string KeyName { get; set; }
+
+        /// <summary>
+        /// Original file path.
+        /// </summary>
+        [JsonProperty("file_path")]
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// File size on disk.
+        /// </summary>
+        [JsonProperty("size")]
+        public long Size { get; set; }
+
+        /// <summary>
+        /// Last modified date.
+        /// </summary>
+        [JsonProperty("last_modified")]
+        public DateTime LastModified { get; set; }
+
+        /// <summary>
+        /// File Hash.
+        /// </summary>
+        [JsonProperty("md5")]
+        public string MD5 { get; set; }
+
+        /// <summary>
+        /// Serialize FileManifest to JSON string
+        /// </summary>
+        /// <returns></returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
