7600dd0f101f238aa523bb56c851b74f27d162d8
Theofanis Tokas <tokastheofanis@gmail.com>
2015-01-16 16:33:58 +0200

    new message class, RestartAppMessage.

---
+++ DeepfreezeApp/Messages/RestartAppMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+namespace DeepfreezeApp
+{
+    [Export(typeof(IRestartAppMessage))]
+    public class RestartAppMessage : IRestartAppMessage
+    {
+        public bool RestartNeeded { get; set; }
+    }
+}
