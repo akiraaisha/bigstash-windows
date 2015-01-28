0ccadab9f2d989551acf681f5b31cb5488a214dc
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-01-28 14:30:24 +0200

    switch to System.Runtime.Serialization in S3Info. Cleanup.

--- DeepfreezeModel/S3Info.cs
+++ DeepfreezeModel/S3Info.cs
@@ -1,52 +1,30 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace DeepfreezeModel
 {
+    [DataContract]
     public class S3Info
     {
-        [JsonProperty("bucket")]
+        [DataMember(Name="bucket")]
         public string Bucket { get; set; }
 
-        [JsonProperty("prefix")]
+        [DataMember(Name="prefix")]
         public string Prefix { get; set; }
 
-        [JsonProperty("token_expiration")]
+        [DataMember(Name="token_expiration")]
         public DateTime TokenExpiration { get; set; }
 
-        [JsonProperty("token_session")]
+        [DataMember(Name="token_session")]
         public string TokenSession { get; set; }
 
-        [JsonProperty("token_uid")]
+        [DataMember(Name="token_uid")]
         public string TokenUID { get; set; }
 
-        [JsonProperty("token_secret_key")]
+        [DataMember(Name="token_secret_key")]
         public string TokenSecretKey { get; set; }
 
-        [JsonProperty("token_access_key")]
+        [DataMember(Name="token_access_key")]
         public string TokenAccessKey { get; set; }
-
-        public S3Info() { }
-
-        public S3Info(JObject json)
-        {
-            this.Bucket = (string)json["bucket"];
-            this.Prefix = (string)json["bucket"];
-            this.TokenExpiration = (DateTime)json["token_expiration"];
-            this.TokenSession = (string)json["token_session"];
-            this.TokenUID = (string)json["token_uid"];
-            this.TokenSecretKey = (string)json["token_secret_key"];
-            this.TokenAccessKey = (string)json["token_access_key"];
-        }
-
-        public string ToJson()
-        {
-            return JsonConvert.SerializeObject(this);
-        }
     }
 }
