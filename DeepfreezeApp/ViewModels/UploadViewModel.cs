cd5f54c9ff7421bfcb46da4a17ef5c43376a7af2
Theofanis Tokas <tokastheofanis@gmail.com>
2015-02-02 19:27:01 +0200

    Changes to handle corrupted local upload files.
    - If LocalUpload.Status = Corrupted then the upload shows an error and can only be deleted.
    - Uploads don't get automatically deleted when the archive does not exist. This covers corrupted status local uploads as well.
    - Saving the local file is done only if LocalUpload.Status is different than Corrupted or NotFound.
    - PrepareUploadAsync is now called when activating the viewmodel if the LocalUpload.Url is not empty (previously supported mode) or if LocalUpload.Status = Corrupted.

--- DeepfreezeApp/ViewModels/UploadViewModel.cs
+++ DeepfreezeApp/ViewModels/UploadViewModel.cs
@@ -849,7 +849,10 @@ namespace DeepfreezeApp
         /// <returns></returns>
         private async Task<bool> SaveLocalUpload(bool useAsync = true)
         {
-            if (this.IsBusy || this.LocalUpload == null)
+            if (this.IsBusy || 
+                this.LocalUpload == null ||
+                this.LocalUpload.Status == Enumerations.Status.Corrupted.GetStringValue() ||
+                this.LocalUpload.Status == Enumerations.Status.NotFound.GetStringValue())
                 return false;
 
             try
@@ -935,7 +938,14 @@ namespace DeepfreezeApp
             try
             {
                 if (this.LocalUpload == null)
-                    throw new Exception("No local upload file found.");
+                {
+                    throw new Exception("No local upload file found");
+                }
+
+                if (this.LocalUpload.Status == Enumerations.Status.Corrupted.GetStringValue())
+                {
+                    throw new Exception(Properties.Resources.ErrorInitializingUploadLogText);
+                }
 
                 if (this.Upload == null || this._isRefreshing)
                 {
@@ -998,11 +1008,26 @@ namespace DeepfreezeApp
 
                 this.ErrorMessage = Properties.Resources.ErrorPreparingUploadGenericText;
 
-                // if the archive doesn't exist on the server, simply remove the upload from the client.
-                if (this.LocalUpload != null && this.Upload != null && this.Archive == null)
+                if (this.LocalUpload != null && this.Archive == null)
                 {
-                    this.RemoveUpload();
+                    if (this.LocalUpload.Status != Enumerations.Status.Corrupted.GetStringValue())
+                    {
+                        this.LocalUpload.Status = Enumerations.Status.NotFound.GetStringValue();
+                    }
+
+                    this.Archive = new Archive()
+                    {
+                        Size = 1,
+                        Key = Path.GetFileNameWithoutExtension(this.LocalUpload.SavePath)
+                    };
+
+                    this.OperationStatus = Enumerations.Status.Error;
                 }
+                // if the archive doesn't exist on the server, simply remove the upload from the client.
+                //if (this.LocalUpload != null && this.Upload != null && this.Archive == null)
+                //{
+                //    this.RemoveUpload();
+                //}
 
                 // this may get thrown when calculating the actual uploaded size to S3.
                 if (e is AmazonS3Exception)
@@ -1443,8 +1468,11 @@ namespace DeepfreezeApp
             // PrepareUploadAsync handles it's exceptions
             // and updates the UI so there's no need to
             // use a try-catch block here.
-            if (!String.IsNullOrEmpty(this.LocalUpload.Url))
+            if (!String.IsNullOrEmpty(this.LocalUpload.Url) ||
+                this.LocalUpload.Status == Enumerations.Status.Corrupted.GetStringValue())
+            {
                 await this.PrepareUploadAsync().ConfigureAwait(false);
+            }
 
         }
 
