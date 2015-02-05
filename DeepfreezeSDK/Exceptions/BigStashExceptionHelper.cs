9c3a3400a104d1cf3d90a40f7ddd7cc1cdccfd12
Theofanis Tokas <tokastheofanis@gmail.com>
2015-02-05 11:54:43 +0200

    Add BigStashExceptionHelper static class.
    This class exposes TryGetBigStashExceptionInformation which was moved from UploadViewModel class in DeepfreezeApp project. This method returns a string with the exception's HttpRequest's information.

---
+++ DeepfreezeSDK/Exceptions/BigStashExceptionHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepfreezeSDK.Exceptions
+{
+    public static class BigStashExceptionHelper
+    {
+        /// <summary>
+        /// Gets the request body of the sent request which resulted in the BigStashException throw.
+        /// </summary>
+        /// <param name="bgex"></param>
+        /// <returns></returns>
+        public static string TryGetBigStashExceptionInformation(Exception ex)
+        {
+            if (ex is BigStashException)
+            {
+                var bgex = ex as BigStashException;
+                var request = bgex.Request;
+
+                if (request != null)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine();
+                    sb.AppendLine("Error Type: " + bgex.ErrorType);
+                    sb.AppendLine("Error Code: " + bgex.ErrorCode);
+                    sb.AppendLine("Status Code: " + bgex.StatusCode);
+                    sb.AppendLine("Failed Request:");
+                    sb.Append("    " + request.ToString().Replace(Environment.NewLine, Environment.NewLine + "        "));
+
+                    return sb.ToString();
+                }
+
+                return "";
+            }
+
+            return "";
+        }
+    }
+}
