cfbee4e5bf44777050725f58855df585eb3fda83
Theofanis Tokas <tokastheofanis@gmail.com>
2015-02-06 11:05:06 +0200

    bump version to 1.4.1.3

--- DeepfreezeApp/Properties/AssemblyInfo.cs
+++ DeepfreezeApp/Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1.2")]
-[assembly: AssemblyFileVersion("1.4.1.2")]
+[assembly: AssemblyVersion("1.4.1.3")]
+[assembly: AssemblyFileVersion("1.4.1.3")]
 // Log4Net configuration
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Log4Net.config", Watch = true)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
