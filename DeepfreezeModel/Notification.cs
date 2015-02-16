b7c49e0eadee279c33d936361fcffcc08bdeacdc
Theofanis Tokas <tokastheofanis@gmail.com>
2015-02-16 18:00:45 +0200

    add notification model.

---
+++ DeepfreezeModel/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DeepfreezeModel
+{
+    public class Notification
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("verb")]
+        public string Verb { get; set; }
+
+        [JsonProperty("created")]
+        public DateTime CreationDate { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+}
