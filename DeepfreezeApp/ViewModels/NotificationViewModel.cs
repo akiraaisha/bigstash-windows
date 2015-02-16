4f2998b9e3d99c0cbd684273afd7028228c08364
Theofanis Tokas <tokastheofanis@gmail.com>
2015-02-16 18:02:56 +0200

    implement INotificationViewModel viewmodel and view.

---
+++ DeepfreezeApp/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+using DeepfreezeModel;
+
+using Caliburn.Micro;
+
+namespace DeepfreezeApp
+{
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [Export(typeof(INotificationViewModel))]
+    public class NotificationViewModel : PropertyChangedBase, INotificationViewModel
+    {
+        private Notification _notification;
+
+        public  string CreationDateText
+        {
+            get
+            {
+                return this._notification.CreationDate.ToString();
+            }
+        }
+        public Notification Notification
+        {
+            get { return this._notification; }
+            set 
+            {
+                this._notification = value; 
+                NotifyOfPropertyChange(() => this.Notification); 
+            }
+        }
+    }
+}
