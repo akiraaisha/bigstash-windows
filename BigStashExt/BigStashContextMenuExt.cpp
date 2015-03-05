f481e7aa62aefc2361587297b7ca185808ba66f5
Theofanis Tokas <tokastheofanis@gmail.com>
2015-03-05 15:58:04 +0200

    Move to_utf8 declaration before OnStashClick.

--- BigStashExt/BigStashContextMenuExt.cpp
+++ BigStashExt/BigStashContextMenuExt.cpp
@@ -269,6 +269,43 @@ IFACEMETHODIMP CBigStashContextMenuExt::InvokeCommand(LPCMINVOKECOMMANDINFO lpcm
 	return S_OK;
 }
 
+///////////////////////////////////////////////////////////////////////////// 
+// Some helper methods
+//
+
+std::string to_utf8(const wchar_t* buffer, int len)
+{
+	int nChars = ::WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		buffer,
+		len,
+		NULL,
+		0,
+		NULL,
+		NULL);
+	if (nChars == 0) return "";
+
+	std::string newbuffer;
+	newbuffer.resize(nChars);
+	::WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		buffer,
+		len,
+		const_cast< char* >(newbuffer.c_str()),
+		nChars,
+		NULL,
+		NULL);
+
+	return newbuffer;
+}
+
+std::string to_utf8(const std::wstring& str)
+{
+	return to_utf8(str.c_str(), (int)str.size());
+}
+
 
 ///////////////////////////////////////////////////////////////////////////// 
 // CBigStashContextMenuExt methods 
@@ -410,42 +447,4 @@ size_t CBigStashContextMenuExt::ExecuteProcess(std::wstring fullPathToExe, std::
 	CloseHandle(piProcessInfo.hThread);
 
 	return iReturnVal;
-}
-
-
-///////////////////////////////////////////////////////////////////////////// 
-// Some helper methods
-//
-
-std::string to_utf8(const wchar_t* buffer, int len)
-{
-	int nChars = ::WideCharToMultiByte(
-		CP_UTF8,
-		0,
-		buffer,
-		len,
-		NULL,
-		0,
-		NULL,
-		NULL);
-	if (nChars == 0) return "";
-
-	std::string newbuffer;
-	newbuffer.resize(nChars);
-	::WideCharToMultiByte(
-		CP_UTF8,
-		0,
-		buffer,
-		len,
-		const_cast< char* >(newbuffer.c_str()),
-		nChars,
-		NULL,
-		NULL);
-
-	return newbuffer;
-}
-
-std::string to_utf8(const std::wstring& str)
-{
-	return to_utf8(str.c_str(), (int)str.size());
 }
