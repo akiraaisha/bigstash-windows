07ef85c814183cbd3dde03d30ddd7f7193add3bc
Theofanis Tokas <tokastheofanis@gmail.com>
2015-03-05 14:34:15 +0200

    Add new ATL project (BigStashExt) to implement the shell context menu extension.

---
+++ BigStashExt/dllmain.h
@@ -0,0 +1,9 @@
+// dllmain.h : Declaration of module class.
+
+class CBigStashExtModule : public ATL::CAtlDllModuleT< CBigStashExtModule >
+{
+public :
+	DECLARE_LIBID(LIBID_BigStashExtLib)
+};
+
+extern class CBigStashExtModule _AtlModule;
