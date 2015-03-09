204e02648a59bee11c41fd6d41c31c292736de31
Theofanis Tokas <tokastheofanis@gmail.com>
2015-03-09 12:53:29 +0200

    NotificationMessage now exposes a Enumerations.NotificationStatus property.

--- DeepfreezeApp/Interfaces/INotificationMessage.cs
+++ DeepfreezeApp/Interfaces/INotificationMessage.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DeepfreezeModel;
+
 namespace DeepfreezeApp
 {
     public interface INotificationMessage
     {
         string Message { get; set; }
+        Enumerations.NotificationStatus NotificationStatus { get; set; }
     }
 }
