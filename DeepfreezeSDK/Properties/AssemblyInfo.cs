e3ba497317c40b4e0f923e44c28991261d1a2fab
Theofanis Tokas <tokastheofanis@gmail.com>
2015-03-31 13:21:26 +0300

    Change assembly name and default namespace to 'BigStash.SDK'.

--- DeepfreezeSDK/Properties/AssemblyInfo.cs
+++ DeepfreezeSDK/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeepfreezeSDK")]
+[assembly: AssemblyTitle("BigStash.SDK")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DeepfreezeSDK")]
+[assembly: AssemblyProduct("BigStash.SDK")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
